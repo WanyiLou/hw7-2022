993a3d28d6c5bc1a2e6cb5fa11dd10a8e7b19d36
Wanyi Lou <112544100+WanyiLou@users.noreply.github.com>
2023-03-29 03:35:05 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,78 @@
+var video = document.querySelector("video");
+
+video.autoplay = false;
+video.loop = false;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+	if (!video.autoplay) {
+		console.log("Auto play is set to false");
+	}
+	if (!video.loop) {
+		console.log("Loop is set to false");
+	}
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");	
+	video.play();
+	video.volume = 1.0;
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");	
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow down Video");	
+	video.playbackRate *= 0.9;
+	console.log("Speed is " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Slow down Video");	
+	video.playbackRate /= 0.9;
+	console.log("Speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip ahead");	
+	video.currentTime += 10;
+	if (video.currentTime >= video.duration) {
+		video.currentTime = 0;
+	}
+	console.log("Video current time is " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		console.log("Unmute");
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	}
+	else {
+		console.log("Mute");
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+});
+
+var slider = document.querySelector("#slider");
+var output = document.querySelector("#volume");
+
+slider.addEventListener("change", function() {
+	output.innerHTML = this.value + "%";
+	video.volume = this.value * 0.01;
+	console.log("The current value is " + video.volume);
+	//console.log('<span id = "volume">'+ output.innerHTML +'</span>');
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.className = "video oldSchool";
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.className = "video";
+});
+
